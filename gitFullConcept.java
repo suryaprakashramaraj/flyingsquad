b59d4e419a4094f32608b2de36ac1bc419a86416
suryaprakashramaraj <711suryaprakash@gmail.com>
2021-09-22 19:49:35 +0530

    flying squad project created

---
+++ gitFullConcept.java
@@ -0,0 +1,13 @@
+package gitSelenium;
+
+public class gitFullConcept {
+
+	public static void main(String[] args) {
+		
+		System.out.println("surya");
+		System.out.println("ravi");
+		System.out.println("sujith");
+		
+	}
+
+}
